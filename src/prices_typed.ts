058ef5ce131055a69c317bb56157062ae5e92e31
hafezfhmi <hafezfahmi2000@gmail.com>
2024-03-29 17:34:04 +0800

    refactor: pass date2 in isMonday as parameter; use date2 to check if its Monday

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -89,14 +89,14 @@ function createApp(database: Database) {
 
   function calculateReduction(date: Date | undefined, date2: Temporal.PlainDate | undefined) {
     let reduction = 0;
-    if (date && isMonday(date) && !isHoliday(date)) {
+    if (date && date2 && isMonday(date, date2) && !isHoliday(date)) {
       reduction = 35;
     }
     return reduction;
   }
 
-  function isMonday(date: Date) {
-    return date.getDay() === 1;
+  function isMonday(date: Date, date2: Temporal.PlainDate) {
+    return date2.dayOfWeek === 1;
   }
 
   function isHoliday(date: Date | undefined) {
