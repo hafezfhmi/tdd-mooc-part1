6a313172d50e249c2685149b3c3cd139e18cf0cf
hafezfhmi <hafezfahmi2000@gmail.com>
2024-03-29 17:13:36 +0800

    refactor: add date2 as parameter for calculateCostForDayTicket

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -48,7 +48,7 @@ function createApp(database: Database) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
-      return calculateCostForDayTicket(age, date, baseCost);
+      return calculateCostForDayTicket(age, date, baseCost, date2);
     }
   }
 
@@ -65,7 +65,12 @@ function createApp(database: Database) {
     return baseCost;
   }
 
-  function calculateCostForDayTicket(age: number | undefined, date: Date | undefined, baseCost: number) {
+  function calculateCostForDayTicket(
+    age: number | undefined,
+    date: Date | undefined,
+    baseCost: number,
+    date2: Temporal.PlainDate | undefined,
+  ) {
     let reduction = calculateReduction(date);
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
